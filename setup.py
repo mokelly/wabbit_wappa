3d2c7b6ba4d5fc6d44055527ca56f45e9a57fb59
Michael J.T. O'Kelly <mokelly@gmail.com>
2014-04-09 15:32:41 +0000

    Bump version: 0.1.0 → 0.1.1

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ history = open('HISTORY.rst').read().replace('.. :changelog:', '')
 
 setup(
     name='wabbit_wappa',
-    version='0.1.0',
+    version='0.1.1',
     description='Wabbit Wappa is a full-featured Python wrapper for the Vowpal Wabbit machine learning utility.',
     long_description=readme + '\n\n' + doclink + '\n\n' + history,
     author="Michael J.T. O'Kelly",
